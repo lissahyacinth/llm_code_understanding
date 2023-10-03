c4153459dc26c789a7fa275e9ea0dd201c7924cd
Lissa Hyacinth <lissahyacinth@gmail.com>
2023-10-03 18:26:17 +0100

    Rework all trivial/real data example to work with updated code

---
+++ Code Examples/trivial/dynamic_code/evaluation/blank.py
@@ -0,0 +1,10 @@
+import functools
+import operator
+
+
+def doubles(x: int) -> int:
+    return x**2
+
+
+def sum_list(x: list[int]) -> int:
+    return functools.reduce(operator.add, (eval(f"doubles({y})") for y in x))
---
+++ Code Examples/trivial/dynamic_code/indirect_evaluation/blank.py
@@ -0,0 +1,15 @@
+import functools
+import operator
+
+
+def doubles(x: int) -> int:
+    return x * 2
+
+
+def triples(x: int) -> int:
+    return x * 3
+
+
+def sum_list(x: list[int]) -> int:
+    applied_function = triples
+    return functools.reduce(operator.add, (eval(f"applied_function({y})") for y in x))
